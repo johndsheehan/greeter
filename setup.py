e1bbe085c14d7388077666ca9770dfc3dabfc9bf
John D Sheehan <john.d.sheehan@gmail.com>
2019-08-01 11:33:44 +0100

    sample python3 module
    Signed-off-by: John D Sheehan <john.d.sheehan@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='greeter',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['greeter'],
+    author='John D Sheehan',
+    author_email='john.d.sheehan@gmail.com',
+    keywords=['example'],
+    url='https://github.com/johndsheehan/greeter'
+)
